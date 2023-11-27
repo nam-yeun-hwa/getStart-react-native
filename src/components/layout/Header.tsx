be87e6b8c1bdd150d587179c3b81f72f5a9d6d12
nam-yeun-hwa <nyh2ya@gmail.com>
2023-11-27 15:58:31 +0900

    Feat(Header):Edit, Done 모드 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-function Header(): JSX.Element {
+interface HeaderProps {
+  editMode: boolean;
+  onModeToggle: () => void;
+}
+
+function Header({ editMode, onModeToggle }: HeaderProps): JSX.Element {
   const { top } = useSafeAreaInsets();
   return (
     <>
@@ -10,25 +22,39 @@ function Header(): JSX.Element {
       <StatusBar backgroundColor={"white"} />
       <View style={styles.container}>
         <Text style={styles.fonts}>Checklists</Text>
+
+        <TouchableOpacity onPress={onModeToggle} style={styles.mode}>
+          <View style={styles.mode}>
+            <Text>{editMode ? "Edit" : "Done"}</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   fonts: {
     fontSize: 16,
     color: "#333333",
     fontWeight: "700",
     letterSpacing: 0.5,
   },
-  container: {
-    height: 60,
-    justifyContent: "center",
-    alignItems: "center",
-  },
   statusBarPlaceholder: {
     backgroundColor: "white",
+  },
+  mode: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    height: "100%",
+    justifyContent: "center",
+    paddingHorizontal: 20,
   },
 });
 
---
+++ src/pages/WeeklySchedule.tsx
@@ -0,0 +1,342 @@
+import React, { useState } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
+
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Header from "../components/layout/Header";
+import CarouselMenu from "../components/weeklySchedule/CarouselMenu";
+import WarningNoPost from "../components/warningSign/WarningNoPost";
+import WeekContent from "../components/weeklySchedule/WeekContent";
+import InputBox from "../components/weeklySchedule/InputBox";
+
+function WeeklySchedule() {
+  const [weeklyList, setWeeklyList] = useState([
+    {
+      id: 1,
+      weekNumber: 1,
+      content:
+        "1-Track your ovulation cycle to an idea of when you will be ovulating.",
+      done: false,
+    },
+    {
+      id: 2,
+      weekNumber: 1,
+      content: "1-Study about symptoms of ovulation",
+      done: false,
+    },
+    {
+      id: 3,
+      weekNumber: 1,
+      content: "1-Take folic acid",
+      done: false,
+    },
+    {
+      id: 4,
+      weekNumber: 2,
+      content: "2-Keep a record of your weight ",
+      done: false,
+    },
+    {
+      id: 5,
+      weekNumber: 2,
+      content: "2-Reduce caffeine intake ",
+      done: false,
+    },
+    {
+      id: 6,
+      weekNumber: 2,
+      content: "2-Take folic acid",
+      done: false,
+    },
+    {
+      id: 7,
+      weekNumber: 3,
+      content: "3-Get an at-home pregnancy test ",
+      done: false,
+    },
+    {
+      id: 8,
+      weekNumber: 3,
+      content: "3-Buy superfood that is good for pregnant women",
+      done: false,
+    },
+    {
+      id: 9,
+      weekNumber: 3,
+      content: "3-Take folic acid",
+      done: false,
+    },
+    {
+      id: 10,
+      weekNumber: 4,
+      content: "4-Eat leafy greens to increase iron intake. ",
+      done: false,
+    },
+    {
+      id: 11,
+      weekNumber: 4,
+      content: "4-Schedule an appointment with a OBGYN.",
+      done: false,
+    },
+    {
+      id: 12,
+      weekNumber: 4,
+      content: "4-Take folic acid",
+      done: false,
+    },
+    {
+      id: 13,
+      weekNumber: 5,
+      content: "5-Take folic acid",
+      done: false,
+    },
+    {
+      id: 14,
+      weekNumber: 6,
+      content: "6-Keep a record of your weight ",
+      done: false,
+    },
+    {
+      id: 15,
+      weekNumber: 6,
+      content: "6-Take folic acid",
+      done: false,
+    },
+    {
+      id: 16,
+      weekNumber: 6,
+      content: "6-Create a fiber-rich diet for better bowel movements. ",
+      done: false,
+    },
+    {
+      id: 17,
+      weekNumber: 7,
+      content: "7-Take folic acid",
+      done: false,
+    },
+    {
+      id: 18,
+      weekNumber: 7,
+      content: "7-Create a fiber-rich diet for better bowel movements. ",
+      done: false,
+    },
+    {
+      id: 19,
+      weekNumber: 7,
+      content: "7-Reduce caffeine intake ",
+      done: false,
+    },
+    {
+      id: 20,
+      weekNumber: 8,
+      content: "8-Reduce caffeine intake ",
+      done: false,
+    },
+    {
+      id: 21,
+      weekNumber: 8,
+      content: "8-Eat more calcium. ",
+      done: false,
+    },
+    {
+      id: 22,
+      weekNumber: 9,
+      content: "9-Consult your doctor about increasing the food portion. ",
+      done: false,
+    },
+    {
+      id: 23,
+      weekNumber: 9,
+      content: "9-Do light exercises",
+      done: false,
+    },
+    {
+      id: 24,
+      weekNumber: 9,
+      content: "9-Schedule a visit to the dentist",
+      done: false,
+    },
+    {
+      id: 25,
+      weekNumber: 10,
+      content: "10-Seek medical help if morning sickness is severe. ",
+      done: false,
+    },
+    {
+      id: 26,
+      weekNumber: 10,
+      content:
+        "10-Before having any food, check to see if it's safe to consume during pregnancy. ",
+      done: false,
+    },
+    {
+      id: 27,
+      weekNumber: 10,
+      content:
+        "10-Consider implenting a new diet plan to increase calcium consumption. ",
+      done: false,
+    },
+    {
+      id: 28,
+      weekNumber: 11,
+      content:
+        "11-Ask your doctor about which medication is safe to take during pregnancy. ",
+      done: false,
+    },
+    {
+      id: 29,
+      weekNumber: 11,
+      content: "11-Eat iron-rich foods to prevent anemia. ",
+      done: false,
+    },
+    {
+      id: 30,
+      weekNumber: 11,
+      content: "11-Learn about the Down Syndrome screening. ",
+      done: false,
+    },
+    {
+      id: 31,
+      weekNumber: 12,
+      content:
+        "12-Consider purchasing materinity pillows for your comfort in bed. ",
+      done: false,
+    },
+    {
+      id: 32,
+      weekNumber: 13,
+      content: "13-Consider storing the cord blood for the baby.  ",
+      done: false,
+    },
+    {
+      id: 33,
+      weekNumber: 13,
+      content:
+        "13-Look for symptoms of UTIs, such as itchiness or burning sensation. ",
+      done: false,
+    },
+    {
+      id: 34,
+      weekNumber: 13,
+      content: "13-Get a pair of materinity tights for circulation. ",
+      done: false,
+    },
+    {
+      id: 35,
+      weekNumber: 14,
+      content:
+        "14-Ask your doctor about if you need any supplements and medications. ",
+      done: false,
+    },
+    {
+      id: 36,
+      weekNumber: 14,
+      content: "14-Maintain a well-balanced diet. ",
+      done: false,
+    },
+    {
+      id: 37,
+      weekNumber: 15,
+      content: "15-Buy healthy snacks to catch up your increased appetite.",
+      done: false,
+    },
+  ]);
+
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const weeklyTotal = [...new Set(weeklyList.map((v) => v.weekNumber))];
+  const [editMode, setEditMode] = useState(true);
+
+  /**
+   * @function onSelectWeek
+   * @param select 선택된 week
+   * @description 캐러셀에서 현재 선택된 index
+   */
+  const onSelectWeek = (select: number) => {
+    setCurrentIndex(select);
+  };
+
+  /**
+   * @function onInsert 항목추가
+   * @param text
+   * @description 현재 week list 항목 추가
+   */
+  const onInsert = (text: string) => {
+    const nextId =
+      weeklyList.length > 0 ? Math.max(...weeklyList.map((v) => v.id)) + 1 : 1;
+    const weekItem = {
+      id: nextId,
+      weekNumber: currentIndex,
+      content: text,
+      done: false,
+    };
+
+    setWeeklyList(weeklyList.concat(weekItem));
+  };
+
+  /**
+   * @function onDone
+   * @param id
+   * @description 리스트아이템 완료 체크
+   */
+  const onDone = (id: number) => {
+    console.log(id);
+    const updateDoneItem = weeklyList.map((v) =>
+      v.id === id ? { ...v, done: !v.done } : v
+    );
+
+    setWeeklyList(updateDoneItem);
+  };
+
+  /**
+   * @function onRemove
+   * @param id
+   * @description 리스트 아이템 삭제
+   */
+  const onRemove = (id: number) => {
+    console.log(id);
+    const updateDelItem = weeklyList.filter((v) => v.id !== id);
+    setWeeklyList(updateDelItem);
+  };
+
+  const onModeToggle = () => {
+    setEditMode(!editMode);
+  };
+  return (
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container} edges={["bottom"]}>
+          <KeyboardAvoidingView
+            behavior={Platform.select({ ios: "padding" })}
+            style={styles.avoid}
+          >
+            <Header editMode={editMode} onModeToggle={onModeToggle} />
+            <CarouselMenu data={weeklyTotal} onSelectWeek={onSelectWeek} />
+            {weeklyList.length === 0 ? (
+              <WarningNoPost />
+            ) : (
+              <WeekContent
+                data={weeklyList
+                  .filter((v) => v.weekNumber === currentIndex)
+                  .reverse()}
+                onDone={onDone}
+                onRemove={onRemove}
+              />
+            )}
+            <InputBox onInsert={onInsert} />
+          </KeyboardAvoidingView>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  avoid: {
+    flex: 1,
+  },
+});
+
+export default WeeklySchedule;
